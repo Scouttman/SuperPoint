e891863beeca2afa0873d5823eb0c54dac748c01
scouttman <sctt.frsr@gmail.com>
2021-10-17 18:34:08 +1100

    minimal experiment for tensorflow 2

--- superpoint/simple_experiment.py
+++ superpoint/simple_experiment.py
@@ -1,12 +1,13 @@
 import logging
 import os
-from superpoint.models import get_model
+# from superpoint.models import get_model
 import numpy as np
 from tensorflow.python.ops.numpy_ops.np_math_ops import true_divide
 import yaml
 import matplotlib.pyplot as plt
 from superpoint.models_V2.utils import detector_head, detector_loss, box_nms
 from keras import backend as K
+from tensorflow.keras import Input
 
 # from superpoint.datasets_V2 import get_dataset
 import superpoint.datasets_V2.synthetic_shapes as get_dataset
@@ -29,41 +30,46 @@ def grad(magic_point, model, data, **config):
 
 def loss(magic_point, model, data, **config):
     y_ = {}
-    y_["logits"] = model["logits"](data["image"])
+    # y_["logits"] = model["logits"](data["image"])
+    if config['data_format'] == 'channels_first':
+        image = tf.transpose(data["image"], [0, 3, 1, 2])
+    else:
+        image = data["image"]
+    tmp = model(image)
+    y_["logits"] = tmp[0]
+    y_["prob"] = tmp[1]
     l = magic_point._loss(y_, data, **config)
+    # scope = "loss_scope"
+    # l += tf.reduce_sum(tf.Graph.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES, scope))
     return l
 
 def local_get_model(image, **config):
-    if config['data_format'] == 'channels_first':
-        image = tf.transpose(image, [0, 3, 1, 2])
+    # if config['data_format'] == 'channels_first':
+    #     image = tf.transpose(image, [0, 3, 1, 2])
+    
     model = vgg_backbone(image, **config)
-    model = detector_head(model, **config)
+    model = detector_head(image, model, **config)
     # model = model["logits"]
-    prob = model['prob']
-    if config['nms']:
-        prob = tf.map_fn(lambda p: box_nms(p, config['nms'],
-                                            min_prob=config['detection_threshold'],
-                                            keep_top_k=config['top_k']), prob)
-        model['prob_nms'] = prob
-    pred = tf.cast(tf.greater_equal(prob, config['detection_threshold']), dtype=tf.int32)
-    model['pred'] = pred
+    
     return model
 
 def main(config):
     magic_point = MagicPoint.MagicPointSimple()
     
-    batch_size = 64
+    batch_size = 32
     dataset = get_dataset.SyntheticShapes(**config['data'])
     dataset = dataset.get_tf_datasets()["training"]
-    # dataset = dataset.shuffle(buffer_size=1024).batch(batch_size)
+    dataset = dataset.shuffle(buffer_size=1024).batch(batch_size)
     
     
     config = magic_point.default_config
     config['training'] = True
     config['data_format'] = 'channels_first'
-    image = K.placeholder(shape=(64, None, None, 1))
+    # image = K.placeholder(shape=(batch_size, None, None, 1))
+    # image = Input(shape=(1,None,None), name='input',)
+    image = Input(shape=(1,120,160), name='input',)
     model = local_get_model(image, **config)
-    # model = magic_point._model(next(iter(dataset)), **config)
+    # model = magic_point._model(next(iter(dataset)), **config) # should this be uncommetned
     #dataset.batch(32).element_spec,
     
     
@@ -86,7 +92,7 @@ def main(config):
     # print("Loss test: {}".format(l))
     # # predictions = model(tmp["image"])
     # # descriptor_loss()
-    # optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+    optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
 
     # loss_value, grads = grad(magic_point, model, data, **config)
 
@@ -102,6 +108,7 @@ def main(config):
     # Keep results for plotting
     train_loss_results = []
     train_accuracy_results = []
+    
 
     num_epochs = 201
 
@@ -110,9 +117,9 @@ def main(config):
         epoch_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
 
         # Training loop - using batches of 32
-        #for i, data in enumerate(iter(dataset)):
         for step, data in enumerate(dataset):
-            data = dataset.batch(64)
+            # print(data["keypoints"].shape)
+            # data = dataset.batch(64)
             # pad dimention should remove with batch
             # for key in data.keys():
             #     data[key] = tf.expand_dims(data[key], axis=0)
@@ -126,19 +133,33 @@ def main(config):
             # training=True is needed only if there are layers with different
             # behavior during training versus inference (e.g. Dropout).
             # epoch_accuracy.update_state(data['valid_mask'], model(data["image"], training=True))
-            tmp = magic_point._metrics(model(data["image"]), data, **config)
-            epoch_accuracy.update_state(tmp["precision"])
-            if(i>1000):
+            if config['data_format'] == 'channels_first':
+                image = tf.transpose(data["image"], [0, 3, 1, 2])
+            else:
+                image = data["image"]
+            tmp = magic_point._metrics(model(image), data, **config)
+            # epoch_accuracy.update_state(tmp["precision"])
+            
+            if(step>1000):
                 break
 
+        if(epoch%50 == 0):
+            out = model(image)
+            fig2, axs = plt.subplots(nrows=2, ncols=2) # two axes on figure
+            axs[0,0].imshow(image[0,0])
+            axs[1,0].imshow(out[1][0])
+            axs[1,1].imshow(data['keypoint_map'][0])
+            axs[0,1].imshow(out[2][0])
+            plt.show()
         # End epoch
         train_loss_results.append(epoch_loss_avg.result())
         # train_accuracy_results.append(epoch_accuracy.result())
 
         if epoch % 1 == 0:
-            print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
-                                                                        epoch_loss_avg.result(),0))
-                                                                        #epoch_accuracy.result()))
+            print("Epoch {:03d}: Loss: {:.3f}".format(epoch,
+                                                                        epoch_loss_avg.result()))
+                                                                        #epoch_accuracy.result())
+            print(f"precision:{tmp['precision'].numpy()}\trecall:{tmp['recall'].numpy()}")
 
 if __name__ == "__main__":
     with open("configs/magic-point_shapes.yaml", 'r') as f:
